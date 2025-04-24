3c0b2d592a55650f76ea15746948138c394ed51c
devpmpb <devpmpb@gmail.com>
2025-04-24 19:29:39 -0300

    commit test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,24 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
-}); 
+});
+
+app.get("/usuarios", async (req, res) => {
+  try {
+    const usuarios = await prisma.usuario.findMany();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(500).json({ error: "erro ao buscar usuarios" });
+  }
+});
